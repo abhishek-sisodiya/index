1799e079ac3741103a8a32c00b4ed517347ff72f
abhishek sisodiya <abhisheksisodiya01@gmail.com>
2018-09-05 11:49:01 +0530

    got Index_Number value

--- src/app/maincomp/maincomp.component.ts
+++ src/app/maincomp/maincomp.component.ts
@@ -7,17 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaincompComponent implements OnInit {
 
+  Index_Number;
   FieldType=[];
   LabelName = [];
-   containers = [];
-   ControlIndex = 1;
+  containers = [];
+  ControlIndex = 1;
   ObjectCollection = [];	// json Object
   MDivID; // id of Mdivs
   MaxOfControlIndex = 1;
   BtnApplyID_String = 8;	// substring of BtnApply id
   BtnCancelID_String = 9;	// substring of BtnCancel id
   MDivID_String = 4;	// substring of Mdiv id
-  testhtml="yes"
+  
   noOfLabels = 0;
   noOfFields = 0;
   constructor() { }
@@ -27,13 +28,17 @@ export class MaincompComponent implements OnInit {
 
   forDiv(event): void {
 
-    const target = event.target;
-    this.FieldType[this.noOfFields++] = target.attributes.FieldType.value;
-    this.LabelName[this.noOfLabels++] = target.attributes.label.value;
+    this.FieldType[this.noOfFields++] = event.target.attributes.FieldType.value;
+    this.LabelName[this.noOfLabels++] = event.target.attributes.label.value;
 
     this.containers.push(this.containers.length);
 
-   
+   }
+
+   doBtnApply(event): void {
+
+    this.Index_Number = event.target.attributes.id.value.substring( [this.BtnApplyID_String ] )
+    
    }
 
 }
